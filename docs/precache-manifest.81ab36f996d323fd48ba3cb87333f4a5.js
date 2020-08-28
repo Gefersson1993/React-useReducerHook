7176a35356fea5f4dd2c006a9539037cc53e2298
Gefersson Hinostroza Condor <geferssonraul1993@outlook.com>
2020-08-27 23:57:44 -0500

    docs modificado y agregado

---
+++ docs/precache-manifest.81ab36f996d323fd48ba3cb87333f4a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "381b2dadc637609a7b80124c4da478c9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4a41fab5ac782b146de",
+    "url": "/static/css/main.97a1bf20.chunk.css"
+  },
+  {
+    "revision": "8efdd3901fd4bd069bef",
+    "url": "/static/js/2.9b77d2e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.9b77d2e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4a41fab5ac782b146de",
+    "url": "/static/js/main.51e8404b.chunk.js"
+  },
+  {
+    "revision": "46f04600f5238c3977c7",
+    "url": "/static/js/runtime-main.836c258a.js"
+  }
+]);
